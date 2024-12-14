99d8a60312760d1b2195eb2f41a5fa84a2077b22
kristjan kangro <kristjan.kangro@visma.com>
2024-12-14 14:12:07 +0200

    env variables ...

--- vite-react/src/App.tsx
+++ vite-react/src/App.tsx
@@ -4,23 +4,38 @@ import './App.css';
 import css1 from "./styles/styles1.module.css"
 import css2 from "./styles/styles2.module.css"
 import "./styles/nested.css"
+import {useEffect, useState} from "react";
 
 const logoName = 'train';
 const module = await import(`./images/${logoName}.jpeg`);
-const modules = import.meta.glob<{default: string}>('./images/*.*', {eager: true});
+const modules = import.meta.glob<{ default: string }>('./images/*.*', {eager: true});
 
 function App() {
+    const [posts, setPosts] = useState<{ title: string }[]>([]);
+
+    useEffect(
+        () => {
+            const url = import.meta.env.DEV
+                ? "http://localhost:5173/posts.json"
+                : "https://dummyjson.com/posts?skip=10&limit=10";
+            fetch(url)
+                .then(res => res.json())
+                .then(json => {
+                    setPosts(json.posts);
+                })
+        }
+    );
 
     return (
         <>
-            {Object.values(modules).map(src => <img src={src.default}/>) }
+            {Object.values(modules).map((src, idx) =>
+                <img key={idx} src={src.default} alt={src.default}/>)}
             <div>
                 <img src={module.default} alt="ttc-logo"/>
             </div>
+            <h1>Posts list</h1>
             <ul className="styled-list">
-                <li className="item">1</li>
-                <li className="item">2</li>
-                <li className="item">3</li>
+                {posts.map((val, idx) => <li key={idx}>{val.title}</li>)}
                 <li className="item selected">4</li>
             </ul>
             <div className="item selected">See pole stylitud nested styleg</div>
@@ -32,7 +47,7 @@ function App() {
                     <img src={reactLogo} className="logo react" alt="React logo"/>
                 </a>
             </div>
-            <h1 className={css1.styles1} >Vite + React ...</h1>
+            <h1 className={css1.styles1}>Vite + React ...</h1>
             <h1 className={css2["styles2-another"]}>Vite + React ...</h1>
             <a href="../second-route/">Second route</a>
         </>
--- vite-react/vite.config.ts
+++ vite-react/vite.config.ts
@@ -2,13 +2,25 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react';
 import {resolve} from "path";
 import cssNesting from "postcss-nesting";
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(),
+        topLevelAwait({
+            // The export name of top-level await promise for each chunk module
+            promiseExportName: '__tla',
+            // The function to generate import names of top-level await promise in each chunk module
+            promiseImportName: (i) => `__tla_${i}`,
+        }),],
     build: {
-        target: "esnext",
+        target: "es2015",
         rollupOptions: {
+            output: {
+                manualChunks: {
+                    "vendors": ["react"],
+                }
+            },
             input: {
                 main: resolve(__dirname, "index.html"),
                 second: resolve(__dirname, "second-route/index.html"),
